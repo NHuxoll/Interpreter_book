717fad0c17873f2699a40e7994c2eeebdd598a55
NHuxoll <nils.huxoll@offis.de>
2023-06-10 14:45:18 +0200

    added Parser struct, New, nextToken and ParseProgram to parser; WIP

--- Go/parser/parser.go
+++ Go/parser/parser.go
@@ -1,2 +1,33 @@
 package parser;
 
+import (
+	"Go/monkey/ast"
+	"Go/monkey/lexer"
+	"Go/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p 
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p * Parser) ParseProgram() *ast.Program {
+	return nil 
+}
+
