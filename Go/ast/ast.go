96f4bab8024b81a3b28c80e482c6eedca16d6303
NHuxoll <nils.huxoll@offis.de>
2023-06-10 14:33:06 +0200

    Added LetStatement and Idetifiert to Go AST

--- Go/ast/ast.go
+++ Go/ast/ast.go
@@ -1,4 +1,6 @@
-package ast;
+package ast
+
+import "Go/monkey/token"
 
 type Node interface {
 	TokenLiteral() string
@@ -26,3 +28,19 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
