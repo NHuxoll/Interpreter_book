3ed429270ba342487e3a4db86cbc2f550a9e87e7
Nils Huxoll <nhuxoll@pop-os.localdomain>
2023-06-22 19:25:13 +0200

    finished chapter 2.4 in go

--- Go/parser/parser_test.go
+++ Go/parser/parser_test.go
@@ -6,6 +6,19 @@ import (
 	"Go/monkey/lexer"
 )
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0{
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
 func TestLetStatements(t *testing.T) {
 	input := `
 	let x = 5;
@@ -16,12 +29,12 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t,p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-			len(program.Statements))
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",len(program.Statements))
 	}
 
 	tests := []struct {
