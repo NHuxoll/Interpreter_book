9ac90a1f577a975f916928ed89951f1e3a10b870
NHuxoll <nils-huxoll@web.de>
2023-09-03 19:00:01 +0200

    added Bang Operator Tests

--- Go/evaluator/evaluator.go
+++ Go/evaluator/evaluator.go
@@ -5,12 +5,21 @@ import (
 	"Go/monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -25,4 +34,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
