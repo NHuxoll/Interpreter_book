60fdbf66a978abd0672130e45c36f68b7bbaaf51
NHuxoll <nils-huxoll@web.de>
2023-10-22 16:35:48 +0200

    added boolean evaluation with tests

--- Go/evaluator/.null-ls_720792_evaluator_test.go
+++ Go/evaluator/.null-ls_720792_evaluator_test.go
@@ -60,24 +60,16 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
-		{"1 < 2", true},
-		{"1 > 2", false},
-		{"1 < 1 ", false},
-		{"1 > 1", false},
-		{"1 == 1", true},
-		{"1 != 1", false},
-		{" 1 == 2 ", false},
-		{" 1 != 2", true},
-		{"(1 > 2) == true", false},
-		{"(1 < 2) == true", true},
-		{"(1 > 2) == false", true},
-		{"(1 < 2) == false", false},
-		{"true == true", true},
-		{"true == false", false},
-		{"true != false", true},
-		{"true != true", false},
-		{"false == false", true},
-		{"false != false", false},
+        {"1 < 2", true},
+        {"1 > 2", false},
+        {
+            "1 < 1 ", false
+        },
+        {"1 > 1", false},
+        {"1 == 1", true},
+        {"1 != 1", false},
+        {" 1 == 2 ", false} ,
+        {" 1 != 2", true},
 	}
 
 	for _, tt := range tests {
